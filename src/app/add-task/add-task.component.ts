8a8053f68386bfdf617b0508f3ca3b5aa96afb37
Daniel Stöhr <stoehr-daniel@web.de>
2022-10-05 15:10:20 +0200

    add task - fix user and buttons

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Category } from '../category';
 import { Priority } from '../priority';
 import { TaskService } from '../task.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-add-task',
@@ -11,23 +12,21 @@ import { TaskService } from '../task.service';
 export class AddTaskComponent implements OnInit {
   categories:Category[]=[]
   priorities:Priority[]=[]
+  users:User[]=[]
 
   selected_prio = 0
 
   constructor(private service:TaskService) { 
     this.service.getCategories().subscribe(data => this.categories = data)
     this.service.getPriorities().subscribe(data => this.priorities = data)
+    this.service.getUsers().subscribe(data => this.users = data)
   }
 
   ngOnInit(): void {
   }
 
-  select_prio(event:any,id:number) {
-    this.selected_prio = id
-    console.log(event.target);
-    
-    console.log(this.selected_prio);
-    
+  select_prio(event:MouseEvent,id:number) {
+    this.selected_prio = id  
   }
 
 
