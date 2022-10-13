6c303e7a6ee8e01324c78cb09efc95fb37b46c3f
Florian Appel <florian.appel1@gmail.com>
2022-10-13 09:45:03 +0200

    try users in contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddContactComponent } from '../add-contact/add-contact.component';
+import { User } from '../user';
+import { TaskService } from '../task.service';
+import { TaskData } from '../task-data';
 
 @Component({
   selector: 'app-contacts',
@@ -8,14 +11,26 @@ import { AddContactComponent } from '../add-contact/add-contact.component';
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
+  tasks:TaskData[] = []
+  users:User[]=[]
 
-  constructor(public dialog: MatDialog) { }
+
+  constructor(public dialog: MatDialog, private service:TaskService) {
+    this.service.getUsers().subscribe(data => {
+      this.users = data
+    })
+  }
 
   ngOnInit(): void {
   }
 
   openDialog() {
     this.dialog.open(AddContactComponent, { panelClass: 'my-panel-class' });
+  }
+
+  getUsername(id:number) {
+    const element = this.users.find(user => user.id == id)
+    return element?.username
   }
 
 }
